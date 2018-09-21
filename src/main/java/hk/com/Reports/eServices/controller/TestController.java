7886be2b4129d2f184c072516f0079d8d19a5875
kmaquino <42625430+kmaquino@users.noreply.github.com>
2018-09-21 08:42:26 +0800

    ESGEN008

--- src/main/java/hk/com/Reports/eServices/controller/TestController.java
+++ src/main/java/hk/com/Reports/eServices/controller/TestController.java
@@ -1,10 +1,21 @@
 package hk.com.Reports.eServices.controller;
 
+import hk.com.Reports.eServices.model.ESGEN008;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
 
-//    public
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public ModelAndView indexGet(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("esgen");
+        mav.addObject("esgen",new ESGEN008());
+        return mav;
+    }
 
 }
