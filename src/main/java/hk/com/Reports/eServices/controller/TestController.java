dfe5fbe1b04768dbb24cdc9eda76fafcba816dd5
maricorpalangao <maricorpalangao.pccw@gmail.com>
2018-09-24 14:41:27 +0800

    ESGEN008

--- src/main/java/hk/com/Reports/eServices/controller/TestController.java
+++ src/main/java/hk/com/Reports/eServices/controller/TestController.java
@@ -23,6 +23,10 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -40,10 +44,19 @@ public class TestController {
     @Autowired
     private Environment env;
 
-    @RequestMapping(value = "/generateReport", method = RequestMethod.POST)
-    public ModelAndView addReportPost(@ModelAttribute("report") Report report) throws IOException {
+
+    @RequestMapping(value={"/generateReport"}, method={org.springframework.web.bind.annotation.RequestMethod.POST})
+    public ModelAndView addReportPost(@ModelAttribute("report") Report report)
+            throws IOException, SQLException, JRException, ParseException
+    {
         ModelAndView mav = new ModelAndView();
-        mav.setViewName("addOrEditReport");
+        mav.setViewName("esgen");
+
+        report.setReportId("ESGEN008");
+        report.setTemplateFilename("ESGEN008.jasper");
+        Map<String, Object> parameters = prepareParameters(report.getParameters());
+        System.out.println(parameters.toString());
+        generateReport(report);
         return mav;
     }
 
@@ -53,49 +66,53 @@ public class TestController {
                 getService(ConnectionProvider.class).getConnection();
     }
 
-    private  Map<String, Object> prepareParameters(String parameters){
+
+    private Map<String, Object> prepareParameters(String parameters) throws ParseException {
         Gson gson = new Gson();
-        Type stringStringMap = new TypeToken<Map<String, String>>(){}.getType();
-        return gson.fromJson(parameters, stringStringMap);
+        Type type = new TypeToken<Map<String, String>>(){}.getType();
+        Map<String, Object> myMap = (Map)gson.fromJson(parameters, type);
+
+        myMap.remove("TRANS_DATE");
+        myMap.remove("TRANS_YEAR");
+        myMap.remove("PRINT_DATE");
+        myMap.remove("PRINT_TIME");
+
+        String startdate = (String)myMap.get("startdate");
+        String enddate = (String)myMap.get("enddate");
+
+        myMap.put("startdate", new String(convertDateToString(convertToDate(startdate))));
+        myMap.put("enddate", new String(convertDateToString(convertToDate(enddate))));
+
+        System.out.println(myMap.toString());
+        return myMap;
     }
 
-    private void generateReport(Report report){
-
-      //  Map<String, Object> parameters = prepareParameters(report.getParameters());
-
-        String reportParams = "[{\"REPORT_ID\":\"ESGEN008\"},{\"LOCATION\":\"asd\"},{\"TRANS_DATE\":\"09-21-2018\"},{\"USER_ID\":\"UID-1234\"},{\"TRANS_YEAR\":\"09-21-2018\"},{\"startdate\":\"09-21-2018\"},{\"enddate\":\"09-21-2018\"},{\"sub1_data0\":\"HKS\"},{\"sub1_data1\":\"TD-EKO\"},{\"sub2_data0\":\"EKS\"},{\"sub2_data1\":\"A\"}]";
-
-        Map<String, Object> parameters = prepareParameters(reportParams);
-
-        File reportDir = new File(env.getProperty("report.location") + report.getReportId());
-
-
-        try {
-
-          //  String reportName = "ESGEN001";
-
-            JasperCompileManager.compileReport(reportDir + ".jrxml");
-
-            JasperPrint print = JasperFillManager.fillReport(reportDir + ".jasper", parameters, getDBConnection());
-
-            JRExporter exporter = new JRPdfExporter();
-            exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
-            exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, new FileOutputStream(reportDir + ".pdf"));
-
-            exporter.exportReport();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Error Generating Report", e);
-        }
-
+    private void generateReport(Report report) throws SQLException, JRException, ParseException {
+        Map<String, Object> parameters = prepareParameters(report.getParameters());
+        System.out.println("parameters : " + parameters);
+        String ROOT_FOLDER = "C:\\reports\\ESGEN008\\";
+        JasperPrint jasperPrint = JasperFillManager.fillReport("C:\\reports\\ESGEN008\\" + report.getTemplateFilename(), parameters, getDBConnection());
+        JasperExportManager.exportReportToPdfFile(jasperPrint, "C:\\reports\\ESGEN008\\\\ESGEN008.pdf");
     }
 
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
-    public ModelAndView indexGet(){
+    @RequestMapping(value={"/index"}, method={org.springframework.web.bind.annotation.RequestMethod.GET})
+    public ModelAndView indexGet() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("esgen");
-        mav.addObject("esgen",new ESGEN008());
+        mav.addObject("report", new Report());
         return mav;
+    }
+
+    Date convertToDate(String receivedDate) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+        Date date = format.parse(receivedDate);
+        return date;
+    }
+
+    private String convertDateToString(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        String strDate = dateFormat.format(date);
+        return strDate;
     }
 
 }
