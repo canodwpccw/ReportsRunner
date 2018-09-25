053cdf8c5ee7ca5b289af7dc4def68b66c720d60
kmaquino <42625430+kmaquino@users.noreply.github.com>
2018-09-25 10:37:39 +0800

    Job

---
+++ src/main/java/hk/com/Reports/eServices/scheduler/ReportJob.java
@@ -0,0 +1,20 @@
+package hk.com.Reports.eServices.scheduler;
+
+import hk.com.Reports.eServices.service.ReportService;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+
+public class ReportJob implements Job {
+
+    @Autowired
+    private ReportService reportService;
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+        reportService.testMethod();
+    }
+
+}
