2e6ded1c3cabe512e9dc3aa709bdd65b86ad1105
kmaquino <42625430+kmaquino@users.noreply.github.com>
2018-09-25 10:38:56 +0800

    service

--- src/main/java/hk/com/Reports/eServices/service/ReportService.java
+++ src/main/java/hk/com/Reports/eServices/service/ReportService.java
@@ -1,12 +1,6 @@
 package hk.com.Reports.eServices.service;
 
-
-import hk.com.Reports.eServices.model.Report;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
 public interface ReportService {
-    public void uploadJasperFiles(Report report) throws IOException;
-    public Report saveOrUpdate(Report report);
+
+    public void testMethod();
 }
--- src/main/java/hk/com/Reports/eServices/service/ReportServiceImpl.java
+++ src/main/java/hk/com/Reports/eServices/service/ReportServiceImpl.java
@@ -1,35 +1,12 @@
 package hk.com.Reports.eServices.service;
 
-import hk.com.Reports.eServices.dao.ReportDao;
-import hk.com.Reports.eServices.model.Report;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
 
 @Service
 public class ReportServiceImpl implements ReportService{
-    @Autowired
-    private Environment env;
-    @Autowired
-    private ReportDao reportDao;
 
     @Override
-    public void uploadJasperFiles(Report report) throws IOException {
-        for(MultipartFile mf :report.getMultipartFiles()){
-            File reportDir = new File(env.getProperty("report.location") + report.getReportId());
-            if(!reportDir.exists())reportDir.mkdirs();
-            mf.transferTo(new File(reportDir.getAbsolutePath()+"//"+ mf.getOriginalFilename()));
-        }
+    public void testMethod() {
+        System.out.println("test method only!");
     }
-
-    @Override
-    public Report saveOrUpdate(Report report) {
-//        return reportDao.saveOrUpdate(report);
-        return null;
-    }
-
 }
