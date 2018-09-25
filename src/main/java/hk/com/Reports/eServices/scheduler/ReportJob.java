492f672e38ca2cc049cfeb21d138b618ecb43f35
kmaquino <42625430+kmaquino@users.noreply.github.com>
2018-09-25 16:20:08 +0800

    ESGEN008

--- src/main/java/hk/com/Reports/eServices/scheduler/ReportJob.java
+++ src/main/java/hk/com/Reports/eServices/scheduler/ReportJob.java
@@ -1,22 +1,20 @@
 package hk.com.Reports.eServices.scheduler;
 
 import hk.com.Reports.eServices.service.ReportService;
-import org.quartz.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+
+import java.util.logging.Logger;
 
 @Configuration
 @EnableScheduling
 @EnableAsync
 public class ReportJob {
-    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     ReportService reportService;
