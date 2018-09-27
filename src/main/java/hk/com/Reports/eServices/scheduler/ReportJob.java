dfce82a93ca86919f23ab5b70c107f0bfca79069
kmaquino <42625430+kmaquino@users.noreply.github.com>
2018-09-27 10:36:31 +0800

    Scheduler

--- src/main/java/hk/com/Reports/eServices/scheduler/ReportJob.java
+++ src/main/java/hk/com/Reports/eServices/scheduler/ReportJob.java
@@ -1,20 +1,54 @@
 package hk.com.Reports.eServices.scheduler;
 
+import hk.com.Reports.eServices.model.Report;
 import hk.com.Reports.eServices.service.ReportService;
-import org.quartz.*;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
-public class ReportJob implements Job {
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Configuration
+@EnableScheduling
+@EnableAsync
+public class ReportJob {
 
     @Autowired
-    private ReportService reportService;
+    ReportService reportService;
 
-    @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-
-        reportService.testMethod();
+    @Scheduled(cron = "*/10 * * * * *")
+    public void runTestDaily(){
+        List<Report> dailyReports = reportService.getAllDailyReports();
+        for(Report report:dailyReports){
+            System.out.println("Generating " + report.getReportId() + "...");
+            try {
+                reportService.generatePDF(report,"DAILY");
+                System.out.println( "SUCESS!" );
+            } catch (ParseException e) {
+                System.out.println( "FAILED!" );
+                e.printStackTrace();
+            } catch (SQLException e) {
+                System.out.println( "FAILED!" );
+                e.printStackTrace();
+            } catch (JRException e) {
+                System.out.println( "FAILED!" );
+                e.printStackTrace();
+            }finally {
+                continue;
+            }
+        }
     }
+
+//    @Scheduled(cron = "*/5 * * * * *")
+//    public void runTestWeekly(){
+//        System.out.println("2 test test");
+//        reportService.testMethod();
+//    }
 
 }
